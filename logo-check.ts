50ef1ee3e5bdb4f4a906f8ccdc135ead72ed5f86
fylcr <1435727255@qq.com>
2026-09-13 20:51:00 +0800

    chore: drop a scratch script from the previous commit

--- logo-check.ts
+++
@@ -1,3 +0,0 @@
-import { renderLogo } from './packages/core/src/templates/components/logo.template.ts'
-const html = renderLogo({ size: 'md', variant: 'white', showText: true })
-console.log(html.replace(/\n\s*/g, ' ').trim().slice(0, 420))
